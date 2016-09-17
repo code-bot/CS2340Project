4cec6a40bef3a7f3e0caf01233b885cabe5bafd4
Sahaj Bhatt <sahajb97@gmail.com>
2016-09-17 18:19:27 -0400

    Initial commit: Created basic JavaFX Application and organization packages

---
+++ src/fxapp/MainFXApplication.java
@@ -0,0 +1,15 @@
+package fxapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Sahaj Bhatt on 9/17/16.
+ */
+public class MainFXApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        //TODO: Show window
+    }
+}
